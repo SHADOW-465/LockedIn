27fa442d81979aefcaaa7f8d7f861b842ae79ada
SHADOW-465 <kumaarsk465@gmail.com>
2026-02-19 08:55:01 +0530

    fix: remove extra closing div in tasks page

--- src/app/(dashboard)/tasks/page.tsx
+++ src/app/(dashboard)/tasks/page.tsx
@@ -414,23 +414,22 @@ export default function TasksPage() {
                             </div>
                         )}
                     </div>
-                    </div>
                 )}
-        </div >
+            </div>
 
-            {/* Task Detail Modal */ }
-    {
-        detailTask && (
-            <TaskDetailModal
-                task={detailTask}
-                onClose={() => setDetailTask(null)}
-                onSelfComplete={() => handleCompleteTask(detailTask.id)}
-                onFail={() => handleFailTask(detailTask.id)}
-            />
-        )
-    }
+            {/* Task Detail Modal */}
+            {
+                detailTask && (
+                    <TaskDetailModal
+                        task={detailTask}
+                        onClose={() => setDetailTask(null)}
+                        onSelfComplete={() => handleCompleteTask(detailTask.id)}
+                        onFail={() => handleFailTask(detailTask.id)}
+                    />
+                )
+            }
 
-    <BottomNav />
-            </>
-            )
+            <BottomNav />
+        </>
+    )
 }
